8d2786e3b4382bd4b012611c52c00d7054286ab0
Lorenzo Zardo Danzmann <92864867+lorenzozardo@users.noreply.github.com>
2024-02-10 23:05:16 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Scroll Reveal
+window.sr = ScrollReveal({ reset: false })
+
+sr.reveal('.area-1', { duration: 1000 })
+sr.reveal('.area-2', { duration: 1000 })
+sr.reveal('.area-3', { duration: 1000 })
+sr.reveal('.area-4', { duration: 1500 })
+sr.reveal('.area-5', { duration: 1500 })
+sr.reveal('.area-6', { duration: 1500 })
+sr.reveal('.area-7', { duration: 2000 })
+sr.reveal('.area-8', { duration: 2000 })
+sr.reveal('.area-9', { duration: 2000 })
